0afc5af3024fbdb186f3bc2b773af7d295d76092
U HO YOON J <Bran@juui-macbook-pro.local>
2017-06-06 00:13:54 +0900

    1.1 - Remove Console Print

--- ResultsAnalysis/main.swift
+++ ResultsAnalysis/main.swift
@@ -11,7 +11,7 @@ import Foundation
 // Global Variable.
 let fm = FileManager.default
 var students: [Student]?
-
+var resultString = ""
 
 // Input - Read Student Data.
 let dataFileName = "students.json"
@@ -30,35 +30,30 @@ if fm.isReadableFile(atPath: dataPath.path) {
 }
 
 
-// Result Analysys.
-var resultString = "성적결과표\n\n"
+// Make Result String.
+resultString += "성적결과표\n\n"
 students?.sort(by: { (s1, s2) -> Bool in
     return s1.name < s2.name
 })
 
-// 1. All Student Average.
+// Make Result String. - All Student Average.
 var sum = 0.0
 for stu in students! {
     sum += stu.average
 }
 let allAvg = sum/Double(students!.count)
-print("전체 평균 : \(allAvg.roundToPlaces(places: 2))")
 resultString += "전체 평균 : \(allAvg.roundToPlaces(places: 2))\n\n"
 
-// 2. Each Student GPA
-print("개인별 학점\n")
+// Make Result String. - Each Student GPA
 resultString += "개인별 학점\n"
 for stu in students! {
-    print("\(stu.name!)\t: \(stu.GPA)")
     resultString += "\(stu.name!)\t: \(stu.GPA)\n"
 }
 
-// 3. Pass Students
-print("수료생")
+// Make Result String. - Pass Students
 resultString += "\n수료생\n"
 for stu in students! {
     if stu.average > 70 {
-        print(stu.name!)
         resultString += "\(stu.name!), "
     }
 }
@@ -70,7 +65,7 @@ let resultName = "result.txt"
 let resultPath = fm.homeDirectoryForCurrentUser.appendingPathComponent(resultName)
 
 if fm.createFile(atPath: resultPath.path, contents: resultString.data(using: String.Encoding.utf8), attributes: nil) {
-    print("succeed")
+    print("Succeed")
 } else {
-    print("fail")
+    print("Fail")
 }
